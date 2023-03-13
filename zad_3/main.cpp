3ad3522904175bb94b6de4b55c12a1bf8ef683a5
radekaadek <radekaadek@gmail.com>
2023-03-13 20:40:34 +0100

    zad3

---
+++ zad_3/main.cpp
@@ -0,0 +1,20 @@
+#include <Graph.h>
+#include <Simple_window.h>
+
+// Compile with:
+// g++ -o example main.cpp $(find graph_lib/ -name "*.cpp") -I graph_lib/ -lfltk -lfltk_images -lX11 -lXext -O4 -Wall
+
+int main()
+{
+    Simple_window wnd(Point(100, 100), 600, 400, "Radoslaw Dabkowski (325683)");
+    Graph_lib::Polygon poly;
+    poly.add(Point(100, 100));
+    poly.add(Point(100, 200));
+    poly.add(Point(200, 200));
+    poly.set_color(Color::red);
+    wnd.attach(poly);
+    wnd.wait_for_button();
+    poly.add(Point(200, 100));
+    wnd.wait_for_button();
+    return 0;
+}
