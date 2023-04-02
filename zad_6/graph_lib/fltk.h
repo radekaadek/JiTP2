5f756a18f09259215421fbe18b9a6c2a8c00bf23
radekaadek <radekaadek@gmail.com>
2023-04-02 17:58:23 +0200

    zad_5fix

---
+++ zad_6/graph_lib/fltk.h
@@ -0,0 +1,23 @@
+#ifndef FLTK_GUARD
+#define FLTK_GUARD 1
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wcast-function-type"
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Output.H>
+#include <cstdlib>       // for exit(0)
+#include <FL/fl_draw.H>
+#include <FL/Enumerations.H>
+
+#include <Fl/Fl_JPEG_Image.H>
+#include <Fl/Fl_GIF_Image.H>
+
+#pragma GCC diagnostic pop
+
+#endif
+
