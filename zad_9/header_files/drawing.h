7517bfc59071151338e5fe2d8db518cd87c93075
radekaadek <radekaadek@gmail.com>
2023-05-11 19:28:39 +0200

    moved implementations to cpp

--- zad_9/header_files/drawing.h
+++ zad_9/header_files/drawing.h
@@ -5,7 +5,8 @@
 
 class myWindow : public Graph_lib::Window {
 public:
-    myWindow(Graph_lib::Point loc, int w, int h, const std::string& title, FPoint scale = { 1.0f, 1.0f}, FPoint translation = { 0.0f, 0.0f }) :
+    myWindow(Graph_lib::Point loc, int w, int h, const std::string& title,
+            FPoint scale = { 1.0f, 1.0f}, FPoint translation = { 0.0f, 0.0f }) :
         Graph_lib::Window(loc, w, h, title),
         animate(Graph_lib::Point(0, 0), 70, 20, "Start/Stop", cb_toggleAnimation),
         close(Graph_lib::Point(0, 20), 70, 20, "Close", cb_close),
@@ -23,12 +24,7 @@ public:
             delete f.first;
         }
     };
-    void attach_fig(figure* f)
-    {
-        auto shape = f->get_shape(scale, transformation);
-        figures.push_back(std::make_pair(f, shape));
-        attach(*shape);
-    }
+    void attach_fig(figure* f);
 private:
     Button animate, close;
     FPoint scale, transformation, center;
@@ -44,40 +40,7 @@ private:
     {
         reference_to<myWindow>(pw).hide();
     }
-    void toggleAnimation()
-    {
-        animationRunning = !animationRunning;
-        if (animationRunning) {
-            // change the button label
-            animate.label = "Stop";
-            Fl::add_timeout(0.25, timer_callback, this);
-        }
-        else {
-            Fl::remove_timeout(timer_callback, this);
-            animate.label = "Start";
-        }
-    }
-    static void timer_callback(Address addr)
-    {
-        myWindow* pWnd = static_cast<myWindow*>(addr);
-        pWnd->rotationAngle += 0.01f;
-        pWnd->refreshMap();
-        if (pWnd->animationRunning)
-            Fl::repeat_timeout(0.25, timer_callback, pWnd);
-    }
-    void refreshMap()
-    {
-        for (auto& f : figures) {
-            f.first->rotate(rotationAngle, center);
-            // detach the old shape
-            detach(*f.second);
-            delete f.second;
-            // create a new shape
-            f.second = f.first->get_shape(scale, transformation);
-            // attach the new shape
-            attach(*f.second);
-        }
-        redraw();
-    }
-
+    void toggleAnimation();
+    static void timer_callback(Address addr);
+    void refreshMap();
 };
--- zad_9/source_files/drawing.cpp
+++ zad_9/source_files/drawing.cpp
@@ -1,1 +1,46 @@
 #include "drawing.h"
+
+void myWindow::refreshMap()
+{
+    for (auto& f : figures) {
+        f.first->rotate(rotationAngle, center);
+        // detach the old shape
+        detach(*f.second);
+        delete f.second;
+        // create a new shape
+        f.second = f.first->get_shape(scale, transformation);
+        // attach the new shape
+        attach(*f.second);
+    }
+    redraw();
+}
+
+void myWindow::timer_callback(Address addr)
+{
+    myWindow* pWnd = static_cast<myWindow*>(addr);
+    pWnd->rotationAngle += 0.01f;
+    pWnd->refreshMap();
+    if (pWnd->animationRunning)
+        Fl::repeat_timeout(0.25, timer_callback, pWnd);
+}
+
+void myWindow::toggleAnimation()
+{
+    animationRunning = !animationRunning;
+    if (animationRunning) {
+        // change the button label
+        animate.label = "Stop";
+        Fl::add_timeout(0.25, timer_callback, this);
+    }
+    else {
+        Fl::remove_timeout(timer_callback, this);
+        animate.label = "Start";
+    }
+}
+
+void myWindow::attach_fig(figure* f)
+{
+    auto shape = f->get_shape(scale, transformation);
+    figures.push_back(std::make_pair(f, shape));
+    attach(*shape);
+}
